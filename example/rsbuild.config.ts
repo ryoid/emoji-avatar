473bd77d6e5bad6272d648d9c63e19b8293936fb
Ryan Conceicao <ryoid@users.noreply.github.com>
2024-07-02 23:06:10 +0800

    fix: example title

--- example/rsbuild.config.ts
+++ example/rsbuild.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginBabel } from '@rsbuild/plugin-babel';
-import { pluginSolid } from '@rsbuild/plugin-solid';
+import { defineConfig } from "@rsbuild/core";
+import { pluginBabel } from "@rsbuild/plugin-babel";
+import { pluginSolid } from "@rsbuild/plugin-solid";
 
 export default defineConfig({
-  plugins: [
-    pluginBabel({
-      include: /\.(?:jsx|tsx)$/,
-      exclude: /[\\/]node_modules[\\/]/,
-    }),
-    pluginSolid(),
-  ],
+	html: {
+		title: "Emoji Avatar",
+		meta: {
+			description: "Generate emoji avatars from a string.",
+		},
+	},
+	plugins: [
+		pluginBabel({
+			include: /\.(?:jsx|tsx)$/,
+			exclude: /[\\/]node_modules[\\/]/,
+		}),
+		pluginSolid(),
+	],
 });
